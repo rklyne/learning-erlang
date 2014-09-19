725a0826d98545a64bd5dfcc49e36553140df1d0
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2014-09-19 19:09:27 +0100

    Improved temperature funs

--- temperature.erl
+++ temperature.erl
@@ -3,16 +3,34 @@
 
 -module(temperature).
 -export([mytemp/0,
-         as_kelvin/1]).
+         as_kelvin/1,
+         int_to_celsius/1,
+         int_to_kelvin/1,
+         int_to_farenheit/1]).
 
 % A function definition
 mytemp() -> 
     % A tuple
-    {8, celsius} .
+    int_to_celsius(8).
+
+int_to_celsius(Int) ->
+    {Int, celsius} .
+
+int_to_kelvin(Int) ->
+    {Int, kelvin} .
+
+int_to_farenheit(Int) ->
+    {Int, farenheit} .
+
+% A conversion to celsius
+as_celsius({Temp_f, farenheit}) ->
+    {(Temp_f - 32) *5/9, celsius}.
 
 % A conversion to kelvin
 as_kelvin({Temp_c, celsius}) ->
-    {Temp_c + 253, kelvin}.
+    {Temp_c + 253, kelvin};
+as_kelvin(Temp) ->
+    as_kelvin(as_celsius(Temp)).
 
 
 
